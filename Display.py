974468fcf7930fa98ca045bd7d6647a13919d4c2
timhannafin <timhannafin@gmail.com>
2026-03-11 13:26:52 -0400

    Major refactor
    Begin work to break related classes into their own modules

---
+++ Display.py
@@ -0,0 +1,134 @@
+from adafruit_datetime import datetime, date
+from adafruit_bitmap_font import bitmap_font
+from adafruit_display_shapes.rect import Rect
+from adafruit_display_text import label
+from adafruit_st7789 import ST7789
+import adafruit_imageload
+
+import displayio
+import bitmaptools
+import digitalio
+import board
+import os
+import math
+from fourwire import FourWire
+
+from Config import CONFIG, CONFIG_VALUES
+
+class Display:
+    def __init__(self):
+        self.displayHeight = 135
+        self.displayWidth = 240
+
+        self.displayGroup = displayio.Group()
+        self.alertGroup = displayio.Group()
+        self.textColor = 0xFFFFFF
+        self.bgColor = 0x000000
+        self.font = bitmap_font.load_font("/fonts/Helvetica-Bold-16.bdf")
+        self.led = digitalio.DigitalInOut(board.A2)
+        self.led.direction = digitalio.Direction.OUTPUT
+        self.GOAL_ALERT_COLOR_1 = os.getenv(CONFIG.GOAL_ALERT_COLOR_1)
+        self.GOAL_ALERT_COLOR_2 = os.getenv(CONFIG.GOAL_ALERT_COLOR_2)
+        return
+
+    def init(self):
+        TEXT_COLOR = 0xFFFF00
+
+        # Release any resources currently in use for the displays
+        displayio.release_displays()
+
+        spi = board.SPI()
+        tft_cs = board.A0
+        tft_dc = board.A1
+
+        display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs)
+        self.display = ST7789(
+            display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53
+        )
+
+        self.game_label_background_rectangle = Rect(0, 0, self.displayWidth, math.floor(self.displayHeight*.25), fill=0xFFFF00)
+        self.displayGroup.append(self.game_label_background_rectangle)
+
+        placeholder_text = '#' * 25
+
+        self.game_label = label.Label(self.font, text=placeholder_text, color=0x000000)
+        self.game_label.anchor_point = (0.5, 0.0)
+        self.game_label.anchored_position = (self.displayWidth/2, 10)
+
+        self.time_label = label.Label(self.font, text=placeholder_text, color=self.textColor)
+        self.time_label.anchor_point = (0.0, 0.0)
+        self.time_label.anchored_position = (10, 50)
+
+        self.tv_label = label.Label(self.font, text=placeholder_text, color=self.textColor)
+        self.tv_label.anchor_point = (0.0, 0.0)
+        self.tv_label.anchored_position = (10, 95)
+
+        self.displayGroup.append(self.game_label)
+        self.displayGroup.append(self.time_label)
+        self.displayGroup.append(self.tv_label)
+
+
+        bitmap, self.alertPalette = adafruit_imageload.load(
+            "sprites/goal_sprite.bmp",
+            bitmap=displayio.Bitmap,
+            palette=displayio.Palette
+        )
+        self.alertGroup = group = displayio.Group()
+        tile_grid = displayio.TileGrid(bitmap, pixel_shader=self.alertPalette)
+        self.alertGroup.append(tile_grid)
+        self.led.value = False
+        return
+
+
+    def showInfo(self):
+        self.display.root_group = self.displayGroup
+
+    def showGoalAlert(self):
+        self.led.value = True
+        self.display.root_group = self.alertGroup
+        for i in range( 1, os.getenv(CONFIG.GOAL_ALERT_LENGTH) ):
+            self.alertPalette[0]=self.GOAL_ALERT_COLOR_1
+            self.alertPalette[1]=self.GOAL_ALERT_COLOR_2
+            time.sleep(.25)
+            self.alertPalette[1]=self.GOAL_ALERT_COLOR_1
+            self.alertPalette[0]=self.GOAL_ALERT_COLOR_2
+            time.sleep(.25)
+        self.led.value = False
+        self.showInfo()
+        pass
+
+    def getDateDisplayString(self, displayDatetime):
+        if displayDatetime.date() == datetime.now().date():
+            dateString = 'Today'
+        else:
+            pieces = displayDatetime.ctime().split(' ')
+            dayName = pieces[0]
+            monthName = pieces[1]
+            date = pieces[2] if pieces[2] != '' else pieces[3]
+            dateString = f'{dayName}, {monthName} {date}'
+
+        ampm = 'AM' if displayDatetime.time().hour <= 12 else 'PM'
+        hour = displayDatetime.time().hour if displayDatetime.time().hour <= 12 else displayDatetime.time().hour-12
+        timeString = f'{hour}:{displayDatetime.time().minute:02}{ampm}'
+
+        return f'{dateString} @ {timeString}'
+
+    def getTVDisplayString(self, tvBroadcasts):
+        allowedNetworks = os.getenv('MY_CHANNELS').split(',')
+        channelList = []
+        for tv in tvBroadcasts:
+            if tv['network'] in allowedNetworks:
+                channelList.append(tv['network'])
+        return 'On ' + ', '.join(channelList)
+
+    def setDisplayGameNameText(self, text):
+        self.game_label.text = text
+        return
+
+    def setDisplayGameTimeText(self, gameDatetime):
+        self.time_label.text = self.getDateDisplayString(gameDatetime)
+        return
+
+    def setDisplayTVText(self, channelList):
+        self.tv_label.text = self.getTVDisplayString(channelList)
+        return
--- code.py
+++ code.py
@@ -1,168 +1,19 @@
-from adafruit_bitmap_font import bitmap_font
 from adafruit_datetime import timedelta, timezone, datetime, date
-from adafruit_display_shapes.rect import Rect
-from adafruit_display_text import label
-from adafruit_st7789 import ST7789
-from fourwire import FourWire
 
-import adafruit_imageload
 import adafruit_logging as logging
 import adafruit_ntp
 import adafruit_requests
-import bitmaptools
-import board
-import digitalio
-import displayio
-import ipaddress
-import json
-import math
 import os
-import random
 import rtc
 import socketpool
 import ssl
-import terminalio
 import time
 import wifi
 
-
-class CONFIG_VALUES:
-    false = 'False'
-    true = 'True'
-
-class CONFIG:
-    WIFI_NAME = 'WIFI_NAME'
-    WIFI_PASSWORD = 'WIFI_PASSWORD'
-    WATCH_TEAM_CODE = 'WATCH_TEAM_CODE'
-    MY_CHANNELS = 'MY_CHANNELS'
-    GAME_WATCH_DEBUG_MODE = 'GAME_WATCH_DEBUG_MODE'
-    NTP_SERVER_LIST = 'NTP_SERVER_LIST'
-    SKIP_TIME_SYNC = 'SKIP_TIME_SYNC'
-    SYNC_TIMEZONE_WITH_API = 'SYNC_TIMEZONE_WITH_API'
-    LOCAL_TIME_ZONE = 'LOCAL_TIME_ZONE'
-    LOCAL_TIMEZONE_OFFSET = 'LOCAL_TIMEZONE_OFFSET'
-    TIMEZONE_API = 'TIMEZONE_API'
-    TIMEZONE_API_KEY = 'TIMEZONE_API_KEY'
-    API_BASE = 'API_BASE'
-    GOAL_ALERT_COLOR_1 = 'GOAL_ALERT_COLOR_1'
-    GOAL_ALERT_COLOR_2 = 'GOAL_ALERT_COLOR_2'
-    GOAL_ALERT_LENGTH = 'GOAL_ALERT_LENGTH'
-
-class Display:
-    def __init__(self):
-        self.displayHeight = 135
-        self.displayWidth = 240
-
-        self.displayGroup = displayio.Group()
-        self.alertGroup = displayio.Group()
-        self.textColor = 0xFFFFFF
-        self.bgColor = 0x000000
-        self.font = bitmap_font.load_font("/fonts/Helvetica-Bold-16.bdf")
-        self.led = digitalio.DigitalInOut(board.A2)
-        self.led.direction = digitalio.Direction.OUTPUT
-        self.GOAL_ALERT_COLOR_1 = os.getenv(CONFIG.GOAL_ALERT_COLOR_1)
-        self.GOAL_ALERT_COLOR_2 = os.getenv(CONFIG.GOAL_ALERT_COLOR_2)
-        return
-
-    def init(self):
-        TEXT_COLOR = 0xFFFF00
-
-        # Release any resources currently in use for the displays
-        displayio.release_displays()
-
-        spi = board.SPI()
-        tft_cs = board.A0
-        tft_dc = board.A1
-
-        display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs)
-        self.display = ST7789(
-            display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53
-        )
-
-        self.game_label_background_rectangle = Rect(0, 0, self.displayWidth, math.floor(self.displayHeight*.25), fill=0xFFFF00)
-        self.displayGroup.append(self.game_label_background_rectangle)
-
-        placeholder_text = '#' * 25
-
-        self.game_label = label.Label(self.font, text=placeholder_text, color=0x000000)
-        self.game_label.anchor_point = (0.5, 0.0)
-        self.game_label.anchored_position = (self.displayWidth/2, 10)
-
-        self.time_label = label.Label(self.font, text=placeholder_text, color=self.textColor)
-        self.time_label.anchor_point = (0.0, 0.0)
-        self.time_label.anchored_position = (10, 50)
-
-        self.tv_label = label.Label(self.font, text=placeholder_text, color=self.textColor)
-        self.tv_label.anchor_point = (0.0, 0.0)
-        self.tv_label.anchored_position = (10, 95)
-
-        self.displayGroup.append(self.game_label)
-        self.displayGroup.append(self.time_label)
-        self.displayGroup.append(self.tv_label)
-
-
-        bitmap, self.alertPalette = adafruit_imageload.load(
-            "sprites/goal_sprite.bmp",
-            bitmap=displayio.Bitmap,
-            palette=displayio.Palette
-        )
-        self.alertGroup = group = displayio.Group()
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=self.alertPalette)
-        self.alertGroup.append(tile_grid)
-        self.led.value = False
-        return
-
-
-    def showInfo(self):
-        self.display.root_group = self.displayGroup
-
-    def showGoalAlert(self):
-        self.led.value = True
-        self.display.root_group = self.alertGroup
-        for i in range( 1, os.getenv(CONFIG.GOAL_ALERT_LENGTH) ):
-            self.alertPalette[0]=self.GOAL_ALERT_COLOR_1
-            self.alertPalette[1]=self.GOAL_ALERT_COLOR_2
-            time.sleep(.25)
-            self.alertPalette[1]=self.GOAL_ALERT_COLOR_1
-            self.alertPalette[0]=self.GOAL_ALERT_COLOR_2
-            time.sleep(.25)
-        self.led.value = False
-        self.showInfo()
-        pass
-
-    def setDisplayGameNameText(self, text):
-        self.game_label.text = text
-        return
-
-    def setDisplayGameTimeText(self, text):
-        self.time_label.text = text
-        return
-
-    def setDisplayTVText(self, text):
-        self.tv_label.text = text
-        return
-
-class ApiRequest:
-    def __init__(self, requests, logger=None):
-        self.requests = requests
-        self.logger = logger
-
-    def requestJson(self, url, encoding='utf-8', headers=None):
-
-        if self.logger:
-            self.logger.info(url)
-
-        response = self.requests.request('GET', url, stream=True, headers=headers)
-        data_string = ''
-        try:
-            for p in response.iter_content(chunk_size=1000):
-                data_string += p.decode(encoding)
-            obj = json.loads(data_string)
-        except e:
-            self.logger.error(e)
-            return None
-        response.close()
-        return obj
+from Config import CONFIG, CONFIG_VALUES
+from Display import Display
+from ApiRequest import ApiRequest
+from GameWatcher import GameWatcher
 
 def setup(debug=False, logger=None):
     ssid = os.getenv(CONFIG.WIFI_NAME)
@@ -170,9 +21,7 @@ def setup(debug=False, logger=None):
     wifi.radio.connect(ssid, os.getenv(CONFIG.WIFI_PASSWORD))
     logger.info(f'Connected to {ssid}')
     logger.info(f'My IP address is {wifi.radio.ipv4_address}')
-    global pool
     pool = socketpool.SocketPool(wifi.radio)
-    # global requests
     requests = adafruit_requests.Session(pool, ssl.create_default_context())
 
     global apiRequest
@@ -289,97 +138,9 @@ class GameWaiter:
 
             time.sleep(sleepTime)
 
-
-class GameWatcher:
-    def __init__(self, display, game, team, apiRequest, logger=None, debug=False):
-        self.game = game
-        self.teamScore = None
-        self.team = team
-        self.display = display
-        self.active = False
-        self.prevGameState = None
-        self.TeamHomeAway = 'awayTeam' if game['awayTeam']['abbrev'] == self.team else 'homeTeam'
-        self.apiRequest = apiRequest
-        self.debug = debug
-        API_BASE = os.getenv(CONFIG.API_BASE)
-        self.boxScoreApiUrl = f'{API_BASE}/gamecenter/{game['id']}/boxscore'
-        self.scheduleApiUrl = f'{API_BASE}/club-schedule/{self.team}/week/{game['gameDate']}'
-        self.logger = logger
-        self.gameState = 'FUT'
-
-    def Watch(self):
-        if self.logger:
-            self.logger.info(f'Watcher started.')
-
-        self.active = True
-        while self.active:
-            #self.readScoreFromBoxScore()
-            self.readScoreFromSchedule()
-
-            if self.gameState == 'FINAL':
-                return
-            time.sleep(1)
-
-    def processScore(self, latestScore):
-        if self.debug is True:
-            if random.randrange(0, 9) == 0:
-                latestScore = self.teamScore + 1
-            pass
-
-        if self.teamScore == None: #This is our first time through, initialize to the current score without alerting
-            self.teamScore = latestScore
-        else:
-            if latestScore > self.teamScore:
-                if self.logger:
-                    self.logger.info(f'Goal Scored')
-                self.display.showGoalAlert()
-                self.teamScore = latestScore
-        return
-
-    def readScoreFromSchedule(self):
-        schedule = apiRequest.requestJson(self.scheduleApiUrl)
-        if schedule != None:
-            game = schedule['games'][0]
-            latestScore = game[self.TeamHomeAway]['score'] if 'score' in game[self.TeamHomeAway] else 0
-            self.processScore(latestScore)
-            self.gameState = game['gameState']
-        return
-
-    def readScoreFromBoxScore(self):
-        boxScore = apiRequest.requestJson(self.boxScoreApiUrl)
-        if boxScore != None: #if boxScore is None then there was a problem with the API call, try again on the next tick
-            latestScore = boxScore[self.TeamHomeAway]['score'] if 'score' in boxScore[self.TeamHomeAway] else 0
-            self.processScore(latestScore)
-            self.gameState = boxScore['gameState']
-        return
-
 def utcToLocalTime(utcDatetime):
     global localTz
     return utcDatetime.replace(tzinfo=localTz) + localTz.utcoffset(utcDatetime)
-
-def getDateDisplayString(displayDatetime):
-    if displayDatetime.date() == datetime.now().date():
-        dateString = 'Today'
-    else:
-        pieces = displayDatetime.ctime().split(' ')
-        dayName = pieces[0]
-        monthName = pieces[1]
-        date = pieces[2] if pieces[2] != '' else pieces[3]
-        dateString = f'{dayName}, {monthName} {date}'
-
-    ampm = 'AM' if displayDatetime.time().hour <= 12 else 'PM'
-    hour = displayDatetime.time().hour if displayDatetime.time().hour <= 12 else displayDatetime.time().hour-12
-    timeString = f'{hour}:{displayDatetime.time().minute:02}{ampm}'
-
-    return f'{dateString} @ {timeString}'
-
-def getTVDisplayString(tvBroadcasts):
-    allowedNetworks = os.getenv('MY_CHANNELS').split(',')
-    channelList = []
-    for tv in tvBroadcasts:
-        if tv['network'] in allowedNetworks:
-            channelList.append(tv['network'])
-    return 'On ' + ', '.join(channelList)
 
 def getNextGame(apiRequest):
     today = utcToLocalTime(datetime.now())
@@ -406,8 +167,8 @@ def setGameDisplayCountdown(display, game):
     startDateTime = utcToLocalTime(startDateTime)
 
     display.setDisplayGameNameText(gameName)
-    display.setDisplayGameTimeText(getDateDisplayString(startDateTime))
-    display.setDisplayTVText( getTVDisplayString(game['tvBroadcasts']))
+    display.setDisplayGameTimeText(startDateTime)
+    display.setDisplayTVText( game['tvBroadcasts'])
     return
 
 def main():
